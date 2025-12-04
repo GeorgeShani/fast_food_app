2be0a0f146d2dc40c44e0beaf5742f5626355098
GeorgeShani <shanidze.geo2004@gmail.com>
2025-12-04 12:49:43 +0400

    feat: add Quicksand font and update layout with new styles and icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fonstLoaded, error] = useFonts({
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+  });
+
+  useEffect(() => { 
+    if (error) throw error;
+    if (fonstLoaded) SplashScreen.hideAsync();
+  }, [fonstLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
