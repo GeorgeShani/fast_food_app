86aea64beb2e9ed30b5923de2db5b02e5e4026f4
GeorgeShani <shanidze.geo2004@gmail.com>
2025-12-05 13:46:04 +0400

    feat: enhance authentication screens with custom inputs and buttons, add appwrite configuration, and define types for menu and user models

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect, Slot } from "expo-router";
 
 export default function HomeLayout() {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
 
   if (!isAuthenticated) {
     return <Redirect href="/sign-in" />;
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.gshan.foodordering",
+  databaseId: "6931aa09002a73964365",
+  userCollectionId: "6931ab0f001cabcd4a28",
+};
